24bbe1c2f3a27e29436232e7cc4b1acb79abb52a
Iris Cacais <112999212+iriscacais@users.noreply.github.com>
2023-09-04 22:58:55 -0300

    atualizando experiencias

--- src/components/Experiencias.js
+++ src/components/Experiencias.js
@@ -10,15 +10,17 @@ class Experiencias extends React.Component {
                 <div className="divExperiencias">
                     <br></br>
                     <ul>
-                    <li>Início do curso de desenvolvimento web na escola de programação Trybe em setembro de 2022.</li>
+                    <li>Curso de desenvolvimento web na escola de programação Trybe de setembro de 2022 até outubro 2023</li>
                    <br></br>
                     <li>Concluído: Módulo de fundamentos <a href="https://www.credential.net/cb5b43d4-d235-45fc-957d-b90026f4b0d7#gs.o7qc1o" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-graduation-cap"></i></a></li>
                     <br></br>
                     <li>Concluído: Módulo de front-end  <a href="https://www.credential.net/bff0f35e-d4f4-4eaa-aa35-e5f43afa138f" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-graduation-cap"></i></a></li>
                     <br></br>
-                    <li>Estudando: Módulo de back-end - Node Js, SQL, Docker e arquitetura de software</li>
+                    <li>Concluído: Módulo de back-end <a href="https://www.credential.net/ab370315-4411-45e2-846b-0bbef4261f8c#gs.5anx54" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-graduation-cap"></i></a></li>
                     <br></br>
-                    <li>Próximos passos: ciência da computação</li>
+                    <li>Concluído: Módulo de ciência da computação <a href="https://www.credential.net/385872df-bdee-4455-92b1-3427762ce1d4#gs.5anwq3" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-graduation-cap"></i></a></li>
+                    <br></br>
+                    <li>Próximos passos: Curso de Vetores e Animação com SVG da Alura</li>
                     </ul>
                 </div>
            </main> 
