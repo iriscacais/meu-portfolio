beb764f69a84e20e2eb035dac9e83a14d1c28146
Iris Cacais <112999212+iriscacais@users.noreply.github.com>
2024-07-06 15:47:19 -0300

    atualiza algum texto

--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -9,7 +9,7 @@ class Sobre extends React.Component {
             <section className="sectionAbout">
               <div className="divImage" id="sobreMim">
                 <h2>Sobre mim</h2>
-                <p>Meu nome é Iris, tenho 30 anos e moro em São Paulo - SP. Sou dev Front - end com conhecimentos em React.js, HTML, CSS, JavaScript e consumo de APIs. Formada na escola de desenvolvimento Trybe  e cursando SASS na Alura.
+                <p>Meu nome é Iris, tenho 30 anos e moro em São Paulo - SP. Sou dev Front - end com conhecimentos em React.js, HTML, CSS, JavaScript e consumo de APIs. Formada na escola de desenvolvimento Trybe.
                 </p>
               </div>
             </section>
